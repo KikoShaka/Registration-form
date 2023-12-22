ad5678cea59c77e6fa48eb74481e1d219b681a90
KikoShaka <119086326+KikoShaka@users.noreply.github.com>
2023-12-22 10:45:57 +0300

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,13 +1,9 @@
 function logout() {
     try {
         sessionStorage.clear();
-        // Тук можете да добавите логика за изчистване на потребителските данни от сесията или локалното съхранение
-
         alert('Logout successful!');
-        // Пренасочване към страницата за вход
         window.location.href = 'login.html';
     } catch (error) {
-        // Обработка на възможни грешки при изхода
         alert('Logout failed: ' + error.message);
     }
 }
